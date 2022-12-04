661af845cb4182f9b2bfd7d78d4c5429d1b36e2b
Masa <mi81ma@gmail.com>
2022-12-04 17:13:53 +0800

    Added libraries for resposible design

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:search_github/first_screen.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   return runApp(DevicePreview(
@@ -16,7 +17,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
+      builder: (context, child) => ResponsiveWrapper.builder(child,
+          maxWidth: 1200,
+          minWidth: 480,
+          defaultScale: true,
+          breakpoints: [
+            const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+            const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+            const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+          ],
+          background: Container(color: const Color(0xFFF5F5F5))),
+      initialRoute: "/",
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       title: 'Flutter Demo',
